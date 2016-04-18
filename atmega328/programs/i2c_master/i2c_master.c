715c7bfe73440ba697ffe661fa8746b4f5a951ab
Engi-xm <kristijonas.a@gmail.com>
2016-04-18 23:06:42 +0300

    switched to underscore_case

--- atmega328/programs/i2c_master/i2c_master.c
+++ atmega328/programs/i2c_master/i2c_master.c
@@ -3,17 +3,17 @@
 #include <util/delay.h>
 
 // outer functions prototypes
-inline void initI2c(void); // initialize i2c bus
-void i2cSend(uint8_t slaveAddr, uint8_t addr, uint8_t *data, uint8_t i); // send i data bytes to slave
-void i2cRead(uint8_t slaveAddr, uint8_t addr, uint8_t i); // read i bytes from slave
+inline void init_i2c(uint16_t freq); // initialize i2c bus
+void i2c_send(uint8_t slave_addr, uint8_t addr, uint8_t *data, uint8_t i); // send i data bytes to slave
+void i2c_read(uint8_t slave_addr, uint8_t addr, uint8_t i); // read i bytes from slave
 // inner functions prototypes
-static inline void i2cStart(void); // send start signal
-static inline void i2cStop(void); // send stop signal
-static inline void i2cSendByte(uint8_t data); // send a byte
-static inline void i2cReadByteAck(void); // receive a byte with acknowledge
-static inline void i2cReadByteNack(void); // receive a byte without acknowledge
+static inline void i2c_start(void); // send start signal
+static inline void i2c_stop(void); // send stop signal
+static inline void i2c_send_byte(uint8_t data); // send a byte
+static inline void i2c_read_byte_ack(void); // receive a byte with acknowledge
+static inline void i2c_read_byte_nack(void); // receive a byte without acknowledge
 
-static inline void initInt0(void);
+static inline void init_int0(void);
 
 // status codes
 #define I2C_START_SENT     0x08 // start signal sent
@@ -31,6 +31,12 @@ static inline void initInt0(void);
 #ifndef I2C_FREQ
 	#define I2C_FREQ 50000 // default to 50kHz
 #endif
+#ifndef I2C_FREQ_MAX
+	#define I2C_FREQ_MAX 100000 // set max freq to 100kHz
+#endif
+#ifndef I2C_FREQ_MIN
+	#define I2C_FREQ_MIN 20000 // set min to 20kHz
+#endif
 #define ARRAY_LENGTH(array) (sizeof(array)/sizeof((array)[0])) // array length macro
 
 #define SLAVE_ADDR 0b1101000
@@ -39,62 +45,62 @@ static inline void initInt0(void);
 
 // structure for rx and tx buffers
 typedef struct buf {
-	uint8_t SlaveAddrBuffer; // slave address buffer
-	uint8_t AddrBuffer; // register address buffer
-	uint8_t BufferLength; // length of data buffer
-} bufferStruct;
+	uint8_t slave_addr_buffer; // slave address buffer
+	uint8_t addr_buffer; // register address buffer
+	uint8_t buffer_length; // length of data buffer
+} buffer_struct;
 // global scope variables
-volatile uint8_t rxBuffer[1]; // receive data buffer
+volatile uint8_t rx_buffer[2]; // receive data buffer
 // file scope variables
-static volatile uint8_t txMode = 1; // selection between tx and rx modes
-static volatile uint8_t isBusy = 0; // status variable
-static volatile uint8_t txBuffer[1]; // transmit data buffer
-static volatile bufferStruct rx; // receive buffer
-static volatile bufferStruct tx; // transmit buffer
+static volatile uint8_t tx_mode = 1; // selection between tx and rx modes
+static volatile uint8_t is_busy = 0; // status variable
+static volatile uint8_t tx_buffer[2]; // transmit data buffer
+static volatile buffer_struct rx; // receive buffer
+static volatile buffer_struct tx; // transmit buffer
 // static volatile uint8_t dummy = 0;
 // static volatile uint8_t inc = 0;
 
 ISR(TWI_vect) {
 	switch(I2C_STATUS) {
 		case I2C_START_SENT:
-			if(txMode) { // if tx mode is selected
-				i2cSendByte(tx.SlaveAddrBuffer); // send trasmit slave address
+			if(tx_mode) { // if tx mode is selected
+				i2c_send_byte(tx.slave_addr_buffer); // send trasmit slave address
 			} else {
-				i2cSendByte(rx.SlaveAddrBuffer); // send receive slave address
+				i2c_send_byte(rx.slave_addr_buffer); // send receive slave address
 			}
 			break;
 		case I2C_MT_SLAW_ACK:
-			i2cSendByte(tx.AddrBuffer); // send register address
+			i2c_send_byte(tx.addr_buffer); // send register address
 			break;
 		case I2C_MT_DATA_ACK:
-			if(tx.BufferLength > 0) { // if havent reached end of tx buffer
-				i2cSendByte(txBuffer[--tx.BufferLength]); // send data from tx buffer
+			if(tx.buffer_length > 0) { // if havent reached end of tx buffer
+				i2c_send_byte(tx_buffer[--tx.buffer_length]); // send data from tx buffer
 			} else { // if reached end of tx buffer
-				i2cStop(); // send stop signal
+				i2c_stop(); // send stop signal
 			}
 			break;
 		case I2C_MR_SLAR_ACK:
-			if(rx.BufferLength > 1) { // if more than one byte needs to be read
-				i2cReadByteAck(); // read byte with ack
+			if(rx.buffer_length > 1) { // if more than one byte needs to be read
+				i2c_read_byte_ack(); // read byte with ack
 			} else {
-				i2cReadByteNack(); // read byte with nack
+				i2c_read_byte_nack(); // read byte with nack
 			}
 			break;
 		case I2C_MR_DATA_ACK:
-			rx.BufferLength--; // decrement to previous byte
-			rxBuffer[rx.BufferLength] = TWDR; // read data
-			if(rx.BufferLength > 1) { // if more than one byte needs to be read
-				i2cReadByteAck(); // read byte with ack
+			rx.buffer_length--; // decrement to previous byte
+			rx_buffer[rx.buffer_length] = TWDR; // read data
+			if(rx.buffer_length > 1) { // if more than one byte needs to be read
+				i2c_read_byte_ack(); // read byte with ack
 			} else {
-				i2cReadByteNack(); // read byte with nack
+				i2c_read_byte_nack(); // read byte with nack
 			}
 			break;
 		case I2C_MR_DATA_NACK:
-			rxBuffer[0] = TWDR; // read data
-			i2cStop(); // send stop signal
+			rx_buffer[0] = TWDR; // read data
+			i2c_stop(); // send stop signal
 			break;
 		default:
-			i2cStop(); // send stop signal
+			i2c_stop(); // send stop signal
 			break;
 	}
 }
@@ -107,80 +113,86 @@ int main() {
 	// setup
 	// clock_prescale_set(clock_div_1);
 	DDRB |= 0xff;
-	uint8_t sendData[1];
-	initI2c();
-	// initInt0();
+	uint8_t send_data[2];
+	init_i2c(I2C_FREQ);
+	// init_int0();
 	sei(); // FUCKING INTERRUPTS
-	sendData[0] = 0b00000000; // TURN THE FUCKING CRYSTAL ON (bit7 == 0)
-	i2cSend(SLAVE_ADDR, REG_ADDR_1, sendData, ARRAY_LENGTH(sendData));
-	sendData[0] = 0b00010000; // set out to 1Hz
-	i2cSend(SLAVE_ADDR, REG_ADDR_2, sendData, ARRAY_LENGTH(sendData));
+	send_data[0] = 0b00000000; // TURN THE FUCKING CRYSTAL ON (bit7 == 0)
+	send_data[1] = 0b00001001;
+	i2c_send(SLAVE_ADDR, REG_ADDR_1, send_data, 2);
+	// send_data[0] = 0b00010000; // set out to 1Hz
+	// i2c_send(SLAVE_ADDR, REG_ADDR_2, send_data, 1);
 	// loop
 	while(1) {
-		i2cRead(SLAVE_ADDR, REG_ADDR_1, 1);
-		_delay_ms(500);
-		PORTB = (rxBuffer[0] >> 4) * 10 + (rxBuffer[0] & 0x0f);
+		i2c_read(SLAVE_ADDR, REG_ADDR_1, 2);
+		_delay_ms(50);
+		PORTB = (rx_buffer[1] >> 4) * 10 + (rx_buffer[1] & 0x0f);
+		_delay_ms(1000);
+		// PORTB = (rx_buffer[0] >> 4) * 10 + (rx_buffer[0] & 0x0f);
+		// _delay_ms(200);
 	}
 		return 0;
 }
 
-static inline void initInt0(void) {
+static inline void init_int0(void) {
 	EICRA |= (1 << ISC01); // trigger on falling edge
 	EIMSK |= (1 << INT0); // enable interrupt
 }
 
-static inline void i2cStart(void) {
+static inline void i2c_start(void) {
 	TWCR = ((1 << TWINT) | (1 << TWSTA) | (1 << TWEN) | (1 << TWIE)); // send start signal
 }
 
-static inline void i2cStop(void) {
+static inline void i2c_stop(void) {
 	TWCR = ((1 << TWINT) | (1 << TWSTO) | (1 << TWEN) | (1 << TWIE)); // send stop signal
-	isBusy = 0; // set to available
-	// dummy = 0;
+	is_busy = 0; // set to available
 }
 
-static inline void i2cSendByte(uint8_t data) {
+static inline void i2c_send_byte(uint8_t data) {
 	TWDR = data; // load data to data register
 	TWCR = ((1 << TWINT) | (1 << TWEN) | (1 << TWIE)); // send byte stored in TWDR
 }
 
-static inline void i2cReadByteAck(void) {
+static inline void i2c_read_byte_ack(void) {
 	TWCR = ((1 << TWINT) | (1 << TWEN) | (1 << TWEA) | (1 << TWIE)); // receive byte, send ack
 }
 
-static inline void i2cReadByteNack(void) {
+static inline void i2c_read_byte_nack(void) {
 	TWCR = ((1 << TWINT) | (1 << TWEN) | (1 << TWIE)); // receive byte, send nack	
 }
 
-inline void initI2c(void) {
-	TWBR = ((F_CPU / I2C_FREQ) - 16) / 2; // set SCL frequency 
+inline void init_i2c(uint16_t freq) {
+	if(freq > I2C_FREQ_MAX || freq < I2C_FREQ_MIN) { // if outside limits
+		freq = I2C_FREQ; // set to default
+	}
+	TWBR = ((F_CPU / freq) - 16) / 2; // set SCL frequency 
 	TWCR |= (1 << TWIE); // enable interrupts
 	TWCR |= (1 << TWEN); // turn on i2c module
 }
 
-void i2cSend(uint8_t slaveAddr, uint8_t addr, uint8_t *data, uint8_t i) {
-	while(isBusy); // check if available
-	isBusy = 1; // set to busy
-	txMode = 1; // set transmit mode
-	tx.SlaveAddrBuffer = (uint8_t)(slaveAddr << 1); // load slave address to buffer
-	tx.AddrBuffer = addr; // load address to buffer
-	tx.BufferLength = i; // get number of bytes to transmit
-	for (uint8_t x = 0; x < i; x++) { // load data to buffer
-		txBuffer[x] = data[x];
+void i2c_send(uint8_t slave_addr, uint8_t addr, uint8_t *data, uint8_t i) {
+	while(is_busy); // check if available
+	is_busy = 1; // set to busy
+	tx_mode = 1; // set transmit mode
+	tx.slave_addr_buffer = (uint8_t)(slave_addr << 1); // load slave address to buffer
+	tx.addr_buffer = addr; // load address to buffer
+	tx.buffer_length = i; // get number of bytes to transmit
+	for (uint8_t x = 0; x < tx.buffer_length; x++) { // load data to buffer
+		tx_buffer[x] = data[x];
 	}
-	i2cStart(); // start transmition
+	i2c_start(); // start transmition
 }
 
-void i2cRead(uint8_t slaveAddr, uint8_t addr, uint8_t i) {
-	i2cSend(slaveAddr, addr, 0, 0);
-	while(isBusy); // check if available
-	isBusy = 1; // set to busy
-	txMode = 0; // set receive mode
-	rx.SlaveAddrBuffer = (uint8_t)((slaveAddr << 1) + 1); // load slave address to buffer
-	// rx.AddrBuffer = addr; // load address to buffer (for book keeping)
-	rx.BufferLength = i; // set number of bytes to receive
+void i2c_read(uint8_t slave_addr, uint8_t addr, uint8_t i) {
+	i2c_send(slave_addr, addr, 0, 0);
+	while(is_busy); // check if available
+	is_busy = 1; // set to busy
+	tx_mode = 0; // set receive mode
+	rx.slave_addr_buffer = (uint8_t)((slave_addr << 1) + 1); // load slave address to buffer
+	// rx.addr_buffer = addr; // load address to buffer (for book keeping)
+	rx.buffer_length = i; // set number of bytes to receive
 	for(uint8_t x = 0; x < i; x++) {
-		rxBuffer[x] = 0; // clear receive buffer
+		rx_buffer[x] = 0; // clear receive buffer
 	}
-	i2cStart(); // start transmition
+	i2c_start(); // start transmition
 }
