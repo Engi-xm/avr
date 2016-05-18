95317a975228b2655f62f0d8acdb2d8866a5ca5e
Engi-xm <kristijonas.a@gmail.com>
2016-05-18 22:36:10 +0300

    minor updates

---
+++ atmega328/programs/nextion_test/nextion_test.c
@@ -0,0 +1,40 @@
+#include <avr/power.h>
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/setbaud.h>
+#include "usart.h"
+
+#define BUTTON0 0x01
+#define BUTTON1 0x02
+
+int main(void) {
+	// setup
+	uint8_t data[7];
+	DDRB = 0xff;
+	init_usart();
+	sei();
+
+	// loop
+	while(1) {
+		usart_read(7);
+		while(usart_rx_busy);
+		for(uint8_t x = 0; x < 7; x++) {
+			data[x] = usart_rx_buffer[x];
+		}
+		switch(data[2]) {
+			case BUTTON1 :
+				PORTB ^= (1 << 1);
+				break;
+			case BUTTON0 :
+				PORTB ^= (1 << 0);
+				break;
+			default :
+				PORTB = 0xf0;
+				break;
+		}
+		_delay_ms(200);
+	}
+
+	return 0;
+}
---
+++ atmega328/programs/nextion_test/usart.h
@@ -0,0 +1,25 @@
+/* Interrupt based USART library. Use functions to communicate via
+USART bus.
+Function init_usart initializes bus (atomic)
+Function usart_send sends data over tx from global buffer array
+Function usart_read reads data from rx and stores to global buffer array
+Library provides gloval flag for waiting for a message*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/setbaud.h>
+
+// outer function prototypes
+void init_usart(void); // initialize usart
+void usart_send(uint8_t *data, uint8_t i); // send message
+void usart_read(uint8_t max_length); // read message
+
+// defines
+#ifndef USART_RX_BUF_LENGTH
+	#define USART_RX_BUF_LENGTH 10 // default to 10
+#endif
+
+// global scope variables
+extern volatile uint8_t usart_rx_buffer[]; // rx buffer
+extern volatile uint8_t usart_tx_buffer[]; // tx buffer
+extern volatile uint8_t usart_rx_busy; // rx busy flag
